6484aa8a898303fdd8a05bad8d69dc4becbb5beb
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-09-20 22:29:03 +0400

    owner

---
+++ src/test/java/config/ApiConfig.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE) // помогает взять следующий путь до файла, если в какой то недоступен
+@Config.Sources({
+        "file:/tmp/api.properties", // абсолютный  путь до файла, предварительно создать файл
+        // и заполнить его данными из api.properties
+        "classpath:api.properties" //файл в папочке resources
+})
+
+public interface ApiConfig extends Config {
+
+    @Key("baseUrl")
+    String baseUrl();
+
+    @Key("token")
+    String token();
+
+}
